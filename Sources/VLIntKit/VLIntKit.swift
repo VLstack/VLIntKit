3eddeb96f4233e27974da527b81fdeddabd3a3e6
VLstack <github@vlstack.fr>
2024-02-22 13:27:28 +0100

    swift tools 5.7 import

--- Sources/VLIntKit/VLIntKit.swift
+++ Sources/VLIntKit/VLIntKit.swift
@@ -1,6 +1,19 @@
-public struct VLIntKit {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
+public
+extension Int
+{
+ var minutesFormatted: String
+ {
+  let hours = self / 60
+  let minutes = self % 60
+  
+  if hours > 0
+  {
+   return String(format: "%dh%02d", hours, minutes)
+  }
+  
+  return String(format: "%dmin", minutes)
+ }
 }
+
