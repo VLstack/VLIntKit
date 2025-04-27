b3cb4d2da287a90297a180e80651acbd645354e1
VLstack <github@vlstack.fr>
2025-04-27 11:45:23 +0200

    refactor

--- Package.swift
+++ Package.swift
@@ -4,13 +4,20 @@
 import PackageDescription
 
 let package = Package(name: "VLIntKit",
+                      defaultLocalization: "en",
                       platforms: [ .iOS(.v17) ],
                       products:
                       [
                        .library(name: "VLIntKit",
                                 targets: [ "VLIntKit" ])
                       ],
+                      dependencies:
+                      [
+                       .package(url: "https://github.com/VLstack/VLBundleKit", from: "1.4.0")
+                      ],
                       targets:
                       [
-                       .target(name: "VLIntKit")
+                       .target(name: "VLIntKit",
+                               dependencies: [ "VLBundleKit" ],
+                               resources: [ .process("Resources") ])
                       ])
--- Sources/VLIntKit/VLIntKit.swift
+++ Sources/VLIntKit/VLIntKit.swift
@@ -1,20 +1,37 @@
 import Foundation
+import VLBundleKit
 
-public extension Int
+extension Int
 {
- var minutesFormatted: String
+ public func toMinutes(short shortFormat: String? = nil,
+                       full fullFormat: String? = nil,
+                       compact: Bool = false) -> String
  {
+  guard self > 60
+  else
+  {
+   let format = shortFormat ?? Bundle.main.localizedString(compact ? "I18N-VLIntKit.shortFormatCompact" : "I18N-VLIntKit.shortFormat",
+                                                           fallbackModule: .module)
+
+   return String(format: format, self)
+  }
+
+  let format = fullFormat ?? Bundle.main.localizedString(compact ? "I18N-VLIntKit.fullFormatCompact" : "I18N-VLIntKit.fullFormat",
+                                                         fallbackModule: .module)
   let hours = self / 60
   let minutes = self % 60
-  
-  if hours > 0
-  {
-   return String(format: "%dh%02d", hours, minutes)
-  }
-  
-  return String(format: "%dmin", minutes)
+
+  return String(format: format, hours, minutes)
  }
- 
+
+ var asHHmm: String
+ {
+  let hour = self / 3600
+  let minute = self / 60 % 60
+
+  return String(format: "%02i:%02i", hour, minute)
+ }
+
  var asHHmmss: String
  {
   let hour = self / 3600
@@ -23,5 +40,16 @@ public extension Int
 
   return String(format: "%02i:%02i:%02i", hour, minute, second)
  }
-}
 
+ @available(*, deprecated, message: ".minutesFormatted is deprecated, use .toMinutes(compact: true) instead")
+ var minutesFormatted: String
+ {
+  self.toMinutes(compact: true)
+ }
+
+ @available(*, deprecated, message: ".toMinutes(shortFormat:) is deprecated, use .toMinutes(short:) instead")
+ func toMinutes(shortFormat: String = "min.") -> String
+ {
+  self.toMinutes(short: shortFormat)
+ }
+}
